79e80ac0f2e24fc661a299039810590d47f7d8dd
Gabriel <gabe.luculescu@gmail.com>
2021-04-22 14:18:32 -0500

    Add initial Drill.cpp

---
+++ chapter-3/Drill.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+/*int main() {
+
+    std::cout << "Enter the name of the person you want to write to (followed by 'enter'):\n";
+    std::string first_name;
+    std::cin >> first_name;
+    
+    std::cout << "Enter the name of a friend (followed by 'enter'):\n";
+    std::string friend_name;
+    std::cin >> friend_name;
+
+    std::cout << "Is your friend male('m') or female('f')?\n";
+    char friend_sex = '0';
+    std::cin >> friend_sex;
+
+    std::cout << "Enter the age of the recipient: ";
+    int age = 0;
+    std::cin >> age;
+
+
+    std::cout << "Dear " << first_name << ",\n\n";
+    std::cout << "How are you? I am fine. Want to hang out sometime?\n";
+
+
+    std::cout << "Have you seen " << friend_name << " lately?\n";
+
+    if(friend_sex == 'm')
+        std::cout << "If you see " << friend_name << " please ask him to call me.\n";
+    if(friend_sex == 'f')
+        std::cout << "If you see " << friend_name << " please ask her to call me.\n";
+        
+    std::cout << "I hear you just had a birthday and you are " << age << " years old.\n";
+    
+    if(age < 12)
+        std::cout << "Next year you will be " << (age+1) << '\n';
+    if(age == 17)
+        std::cout << "Next year you will be able to vote\n";
+    if(age > 70)
+        std::cout << "I hope you are enjoying retirement.\n";
+
+    std::cout << "Yours sincerely,\n";
+    std::cout << "  Gabriel\n";
+
+    return 0;
+}*/
