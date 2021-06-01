a122e4ae501ce3770c8bf137d3cca8dd40aa4aca
Gabriel <gabe.luculescu@gmail.com>
2021-06-01 10:26:20 -0500

    Add initial Drills

---
+++ chapter-10/Drills/Drill-1.cpp
@@ -0,0 +1,11 @@
+/* Chapter 10: Drill 1
+ *
+ *  Start a program to work with points, discussed in §10.4. 
+ *  Begin by defining the data type Point that has two coordinate members x and y.
+ */
+
+struct Point {
+
+    double x;
+    double y;
+};
---
+++ chapter-10/Drills/Drill-3.cpp
@@ -0,0 +1,79 @@
+#include "../include/std_lib_facilities.h"
+
+/* Chapter 10: Drill 3
+ *
+ * Print the data in original_points to see what it looks like. 
+ */
+
+struct Point {
+
+    double x;
+    double y;
+};
+
+std::istream& operator>>(std::istream &is, Point &p) {
+
+    char ch1;
+    char ch2;
+    char ch3;
+    double x;
+    double y;
+    if(is >> ch1 >> x >> ch2 >> y >> ch3 &&
+       ch1 == '(' || ch2 == ',' || ch3 == ')')
+       p = Point{x, y};
+    else {
+        is.clear(std::ios_base::failbit);
+    }
+
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<Point> &p) {
+
+    for(const auto &i : p) {
+        os << '(' << i.x << ',' << i.y << ")\n";
+    }
+
+    return os;
+}
+
+void ignoreLine() {
+
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+
+constexpr int kNum_pairs{7};
+
+int main() {
+
+    try {
+
+        std::cout << "Please enter " << kNum_pairs << " (x,y) coordinate points:\n";
+        std::vector<Point> original_points;
+        for(int i{}; i < kNum_pairs; ++i) {
+            Point p;
+            if(std::cin >> p) {
+                original_points.push_back(p);
+            }
+            else {
+                std::cout << "Oops, that is a incorrect point entered. Try again:\n";
+                std::cin.clear();
+                ignoreLine();
+                --i;
+            }
+        }
+        std::cout << '\n';
+        
+        std::cout << original_points << '\n';
+        return 0; 
+    }
+    catch(const std::exception &e) {
+        std::cerr << "Exception: " << e.what() << '\n';
+        return 1;
+    }
+    catch(...) {
+        std:: cerr << "Exception: something went wrong.\n";
+        return 2;
+    }
+}
